40632483998bfa8a18a2a798c59feaa87261b910
mohibatventurenox <mohib.habib@venturenox.com>
2024-03-18 23:26:19 +0500

    init

--- src/Details.js
+++ src/Details.js
@@ -1,23 +1,17 @@
 // Enter all your detials in this file
 // Logo images
 import logogradient from "./assets/logo.svg";
-import logo from "./assets/logo2.svg";
+// import logo from "./assets/logo2.svg";
 // Profile Image
 import profile from "./assets/profile.jpg";
 // Tech stack images
+import unity from "./assets/techstack/unity.png"
 import html from "./assets/techstack/html.png";
 import css from "./assets/techstack/css.png";
-import sass from "./assets/techstack/sass.png";
 import js from "./assets/techstack/js.png";
-import react from "./assets/techstack/react.png";
-import redux from "./assets/techstack/redux.png";
-import tailwind from "./assets/techstack/tailwind.png";
-import bootstrap from "./assets/techstack/bootstrap.png";
 import vscode from "./assets/techstack/vscode.png";
 import github from "./assets/techstack/github.png";
 import git from "./assets/techstack/git.png";
-import npm from "./assets/techstack/npm.png";
-import postman from "./assets/techstack/postman.png";
 import figma from "./assets/techstack/figma.png";
 // Porject Images
 import projectImage1 from "./assets/projects/project1.jpg";
@@ -30,15 +24,15 @@ import projectImage6 from "./assets/projects/project6.jpg";
 // Logos
 export const logos = {
   logogradient: logogradient,
-  logo: logo,
+  // logo: logo,
 };
 
 // Enter your Personal Details here
 export const personalDetails = {
-  name: "Pavan MG",
-  tagline: "I build things for web",
+  name: "Ibrahim Butt",
+  tagline: "I Develop games for multiple platforms on unity",
   img: profile,
-  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  about: `Yo Bitches`,
 };
 
 // Enter your Social Media URLs here
@@ -52,23 +46,23 @@ export const socialMediaUrl = {
 // Enter your Work Experience here
 export const workDetails = [
   {
-    Position: "Frontend Web Developer",
-    Company: `Company Name here`,
-    Location: "Bengaluru",
+    Position: "Game Developer",
+    Company: `Katana Games`,
+    Location: "Remote",
+    Type: "Full Time",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Associate Engineer",
+    Company: `The Game Storm Studio`,
+    Location: "Lahore",
     Type: "Full Time",
     Duration: "Sep 2021 - Dec 2021",
   },
   {
     Position: "Internship",
-    Company: `Company Name here`,
-    Location: "Bengaluru",
-    Type: "Internship",
-    Duration: "Sep 2021 - Dec 2021",
-  },
-  {
-    Position: "Internship",
-    Company: `Company Name here`,
-    Location: "Bengaluru",
+    Company: `Game Train`,
+    Location: "Lahore",
     Type: "Internship",
     Duration: "Sep 2021 - Dec 2021",
   },
@@ -77,34 +71,21 @@ export const workDetails = [
 // Enter your Education Details here
 export const eduDetails = [
   {
-    Position: "Frontend Development",
-    Company: "Udemy, YouTube, Google, Medium",
-    Location: "Online",
+    Position: "Bachelor in Computer Science",
+    Company: `Gift University`,
+    Location: "Gujranwala",
     Type: "Full Time",
-    Duration: "Jan 2022 - Present",
-  },
-  {
-    Position: "Bachelor in Electronics & Communication",
-    Company: `Your College Name here`,
-    Location: "Bengaluru",
-    Type: "Full Time",
-    Duration: "Aug 2020 - Present",
+    Duration: "Mar 2017 - Mar 2021",
   },
 ];
 
 // Tech Stack and Tools
 export const techStackDetails = {
+  unity: unity,
   html: html,
   css: css,
   js: js,
-  react: react,
-  redux: redux,
-  sass: sass,
-  tailwind: tailwind,
-  bootstrap: bootstrap,
   vscode: vscode,
-  postman: postman,
-  npm: npm,
   git: git,
   github: github,
   figma: figma,
@@ -113,38 +94,34 @@ export const techStackDetails = {
 // Enter your Project Details here
 export const projectDetails = [
   {
-    title: "Project title 1",
+    title: "Triple Hand Poker",
     image: projectImage1,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
+    description: `Multiplayer Turn Based Poker Game`,
+    techstack: "Unity, Photon",
     previewLink: "https://google.com",
     githubLink: "https://github.com",
   },
   {
-    title: "Project title 2",
+    title: "Grand Mafia Shooter",
     image: projectImage2,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
+    description: `Static Shooter Game  with RPG Elements`,
+    techstack: "Unity",
     previewLink: "https://google.com",
     githubLink: "https://github.com",
   },
   {
-    title: "Project title 3",
+    title: "Long Drive Road Trip",
     image: projectImage3,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
+    description: `Simulation of long distance journey `,
+    techstack: "Unity",
     previewLink: "https://google.com",
     githubLink: "https://github.com",
   },
   {
-    title: "Project title 4",
+    title: "Police Dog Simulation",
     image: projectImage4,
-    description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-    techstack: "HTML/CSS, JavaScript",
+    description: `Simulation of Police Dog Missions `,
+    techstack: "Unity",
     previewLink: "https://google.com",
     githubLink: "https://github.com",
   },
@@ -170,6 +147,6 @@ project lorem ipsum generator for dummy content`,
 
 // Enter your Contact Details here
 export const contactDetails = {
-  email: "hi@example.com",
-  phone: "+91 12345 67890",
+  email: "ibrahim.alibu11work@gmail.com",
+  phone: "+92 3137601237",
 };
--- src/Pages/Technologies.js
+++ src/Pages/Technologies.js
@@ -3,19 +3,12 @@ import { techStackDetails } from "../Details";
 
 function Technologies() {
   const {
+    unity,
     html,
     css,
-    js,
-    react,
-    redux,
-    tailwind,
-    bootstrap,
-    sass,
     vscode,
     git,
     github,
-    npm,
-    postman,
     figma,
   } = techStackDetails;
   return (
@@ -29,14 +22,9 @@ function Technologies() {
         </p>
       </section>
       <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
+      <img src={unity} title="html" alt="" />
         <img src={html} title="html" alt="" />
         <img src={css} title="CSS" alt="" />
-        <img src={js} title="JavaScript" alt="" />
-        <img src={react} title="React" alt="" />
-        <img src={redux} title="Redux" alt="" />
-        <img src={tailwind} title="Tailwind CSS" alt="" />
-        <img src={bootstrap} title="Bootstrap" alt="" />
-        <img src={sass} title="SASS" alt="" />
       </section>
       <section>
         <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
@@ -48,8 +36,6 @@ function Technologies() {
         <img src={git} title="Git" alt="Git" />
         <img src={github} title="Github" alt="Github" />
         <img src={figma} title="Figma" alt="Figma" />
-        <img src={npm} title="NPM" alt="NPM" />
-        <img src={postman} title="Postman" alt="Postman" />
       </section>
     </main>
   );
